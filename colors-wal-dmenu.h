a1e2b12121191e3aa3ed2ed81f52d39059d05b4c
Ana Barros <anaiobarros2014@gmail.com>
2022-02-28 00:45:08 +0000

    Add: colors

---
+++ colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#eed387", "#050401" },
+	[SchemeSel] = { "#eed387", "#4A831A" },
+	[SchemeOut] = { "#eed387", "#C78B28" },
+};
